045e69b07e7147e92289123c990511c7aaa639cf
Ben Lambert <ben.c.lambert@gmail.com>
2019-05-16 02:39:23 +0100

    Added shell script file that compiles plots for each dataset

--- src/s_plot_and_changepoint_test.R
+++ src/s_plot_and_changepoint_test.R
@@ -7,24 +7,24 @@ library(RJSONIO)
 
 # command line arguments passed to plotting
 args <- commandArgs(trailingOnly=TRUE)
-data_directory <- as.character(args[1])
-data_filename <- as.character(args[2])
-output_directory <- as.character(args[3])
-output_filename_base <- as.character(args[4])
-plot_unimodal <- as.numeric(args[5])
-plot_unimodal_logged <- as.numeric(args[6])
-plot_unimodal_changepoints <- as.numeric(args[7])
+current_directory <- as.character(args[1])
+data_directory <- as.character(args[2])
+data_filename <- as.character(args[3])
+output_directory <- as.character(args[4])
+output_filename_base <- as.character(args[5])
+plot_raw_logged <- as.numeric(args[6])
+plot_raw_changepoints <- as.numeric(args[7])
 plot_smooth <- as.numeric(args[8])
 plot_pass_threshold <- as.numeric(args[9])
 changepoints_run_length <- as.numeric(args[10])
 changepoints_near_end_threshold <- as.numeric(args[11])
 changepoints_num_tests_commits <- as.numeric(args[12])
 
-setwd(directory)
+setwd(current_directory)
 
 # takes data in form in 'cleaned' folder and preps it for plotting
-f_prep_data <- function(filebase){
-  test <- read.csv(paste0("../cleaned/", filebase, ".csv"), header = F)
+f_prep_data <- function(filebase, data_directory){
+  test <- read.csv(paste0(data_directory, "/", filebase, ".csv"), header = F)
   colnames(test) <- c("year", "month", "day", "hour", "minute", "second", "commit", "score")
   test <- test %>%
     mutate(date_time=make_datetime(year, month, day, hour, minute, second)) %>% 
@@ -36,10 +36,10 @@ f_prep_data <- function(filebase){
 }
 
 # fetch data
-test <- f_prep_data(filename)
+test <- f_prep_data(data_filename, data_directory)
 
 # fit PELT online changepoint model and add means to data frame ----------
-model  <- cpt.mean(test$score, method = "PELT", minseglen = run_length)
+model  <- cpt.mean(test$score, method = "PELT", minseglen = changepoints_run_length)
 cpts <- as.vector(model@cpts)
 if(length(cpts) > 1){
   cpts <- cpts[-length(cpts)]
@@ -47,7 +47,6 @@ if(length(cpts) > 1){
   vMeans <- vector(length = nrow(test))
   ncpts <- length(cpts)
   for(i in 1:(ncpts + 1)){
-    print(i)
     if(i < (ncpts + 1))
       current_cpt <- cpts[i]
     
@@ -61,9 +60,13 @@ if(length(cpts) > 1){
     }
     prev_cpt <- current_cpt
   }
+}else{
+  vMeans <- rep(model@param.est$mean, nrow(test))
 }
 
-cpt_alarm_triggered <- if_else(max(cpts) > near_end_threshold, 1, 0)
+# check if a changepoint has occurred between [changepoints_near_end_threshold tests ago, most recent commit]
+print(changepoints_near_end_threshold)
+cpt_alarm_triggered <- if_else(max(cpts) > (nrow(test) - changepoints_near_end_threshold), 1, 0)
 if(cpt_alarm_triggered == 1){
   max_cpt <- max(cpts)
   cpt_date <- test$date[max_cpt]
@@ -90,9 +93,8 @@ commit_list <- list(cpt_alarm_triggered=cpt_alarm_triggered,
 test$cpt_means <- vMeans
 
 # plot by grouping by every two weeks ----------
-# if unimodal data (most MCMC series plot simple data)
-if(plot_unimodal){
-  g <-
+# raw data plot
+g <-
     test %>% 
     ggplot(aes(x=date_2_week, y=score)) +
     geom_jitter() +
@@ -104,52 +106,54 @@ if(plot_unimodal){
     stat_summary(fun.data=mean_sdl, mult=1, 
                  geom="pointrange", color="red")
   
-  # add log y scale
-  if(plot_unimodal_logged){
-    g <- g +
-      scale_y_log10()
-  }
-  
-  # add changepoints
-  if(plot_unimodal_changepoints){
-    g <- g +
-      geom_line(aes(y=cpt_means), linetype=2, colour="blue", size=1)
-  }
-  
-}else{
-  test <- test %>% 
-    mutate(pass=if_else(score < a_threshold, 1, 0)) %>% 
-    group_by(date_2_week) %>% 
-    summarise(pass=sum(pass),
-              size=n()) %>% 
-    mutate(score=pass / size,
-           upper=qbeta(0.95, 1 + pass, 1 + size - pass),
-           lower=qbeta(0.05, 1 + pass, 1 + size - pass),
-           middle=qbeta(0.5, 1 + pass, 1 + size - pass))
-  
-  g <- 
-    test %>% 
-    ggplot(aes(x=date_2_week, y=score)) +
-    theme_classic() +
-    xlab("Date, two weeks grouped") +
-    ylab("Mean passing threshold") +
-    theme(axis.title = element_text(size=14, colour="black"),
-          axis.text = element_text(size=14, colour="black")) +
-    geom_point() +
-    geom_point(aes(y=middle), colour="red") + 
-    geom_errorbar(aes(ymin=lower, ymax=upper), size=0.1, colour="red") +
-    scale_y_continuous(labels = scales::percent, limits = c(0, 1))
+# add log y scale
+if(plot_raw_logged){
+  g <- g +
+    scale_y_log10()
 }
+
+# add changepoints
+if(plot_raw_changepoints){
+  g <- g +
+    geom_line(aes(y=cpt_means), linetype=2, colour="blue", size=1)
+}
+
+# plot percentage of tests passing user specified threshold
+test <- test %>% 
+  mutate(pass=if_else(score < plot_pass_threshold, 1, 0)) %>% 
+  group_by(date_2_week) %>% 
+  summarise(pass=sum(pass),
+            size=n()) %>% 
+  mutate(score=pass / size,
+         upper=qbeta(0.95, 1 + pass, 1 + size - pass),
+         lower=qbeta(0.05, 1 + pass, 1 + size - pass),
+         middle=qbeta(0.5, 1 + pass, 1 + size - pass))
+
+g1 <- 
+  test %>% 
+  ggplot(aes(x=date_2_week, y=score)) +
+  theme_classic() +
+  xlab("Date, two weeks grouped") +
+  ylab("Mean passing threshold") +
+  theme(axis.title = element_text(size=14, colour="black"),
+        axis.text = element_text(size=14, colour="black")) +
+  geom_point() +
+  geom_point(aes(y=middle), colour="red") + 
+  geom_errorbar(aes(ymin=lower, ymax=upper), size=0.1, colour="red") +
+  scale_y_continuous(labels = scales::percent, limits = c(0, 1))
 
 # add smoothed line
 if(plot_smooth){
   g <- g + 
     geom_smooth(linetype=3, colour="orange")
+  g1 <- g1 + 
+    geom_smooth(linetype=3, colour="orange")
 }
 
 # Output changepoint test results as json to directory -------
 exportJson <- toJSON(commit_list)
-write(exportJson, paste0(output_filename_base, ".json"))
+write(exportJson, paste0(output_directory, "/", output_filename_base, ".json"))
 
 # output plot
-ggsave(paste0(output_filename_base, ".pdf"), g, width = 10, height = 6)
+ggsave(paste0(output_directory, "/", output_filename_base, "_raw.pdf"), g, width = 10, height = 6)
+ggsave(paste0(output_directory, "/", output_filename_base, "_threshold.pdf"), g1, width = 10, height = 6)
